b91e76093ef490b95aa5afea5d786a9e630795a0
cboath <cteater@gmail.com>
2020-10-15 23:55:30 -0500

    Successful location changes

--- scene.py
+++ scene.py
@@ -1,10 +1,13 @@
 import json
 
+
 def getScene(charloc):
     print(charloc)
+    currentloc = [charloc[1], charloc[2]]
     if(charloc[0] == 'f'):
         with open('scenelistforest.json') as forest:
             forestloc = json.load(forest)
-            print(forestloc)
-        
-    
+            for key, value in forestloc.items():
+                for m in range(0, len(value)):
+                    if(value[m]['location'] == currentloc):
+                        print(value[m]['description'])
